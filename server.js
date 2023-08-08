b455f41b95db2811b322ea9994bfcaf5752d20e9
JoeyJung <125326562+JoeyJung@users.noreply.github.com>
2023-08-08 16:52:01 +1000

    POST-form

--- server.js
+++ server.js
@@ -2,7 +2,24 @@
 const express = require('express')
 const app = express()
 
-//launch a server on 8000 port and print message
-app.listen(8000, function(){
-  console.log('listening on 8000')
-});
+//launch a server on 8008 port and print message
+app.listen(8008, function(){
+  console.log('listening on 8008')
+})
+
+// Get HTML file (HOME)
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+
+// })
+
+//send form.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/form.html')
+
+})
+
+// do something when a post request is made to the /add path.
+app.post('/add', (req, res) => {
+  res.send(' transmission completed! ')
+})
