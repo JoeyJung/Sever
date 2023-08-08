a9fdcc5be5dc1c5e51885cd25bb98ba040bc3985
JoeyJung <125326562+JoeyJung@users.noreply.github.com>
2023-08-08 17:37:30 +1000

    Upload file
    Add function to upload file in server (with multer)

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 //basic setting express library
 const express = require('express')
 const app = express()
+const multer = require('multer')
+const fs = require('fs'); 
+
+//use body parser
+app.use(express.urlencoded({extended: true})) 
 
 //launch a server on 8008 port and print message
 app.listen(8008, function(){
@@ -19,7 +24,38 @@ app.get('/', (req, res) => {
 
 })
 
-// do something when a post request is made to the /add path.
-app.post('/add', (req, res) => {
-  res.send(' transmission completed! ')
-})
+// Set up multer storage for uploaded files
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // This is the folder where uploaded files will be stored
+  },
+  filename: function (req, file, cb) {
+    // Generate a unique name for the uploaded file (you can use a package like uuid)
+    const uniqueFilename = Date.now() + '-' + file.originalname;
+    cb(null, uniqueFilename);
+  },
+});
+
+// Create the multer instance using the storage configuration
+const upload = multer({ storage });
+
+// Serve static files in the "uploads" folder
+app.use('/uploads', express.static('uploads'));
+
+// Ensure the "uploads" folder exists, create it if it doesn't
+const uploadDirectory = './uploads';
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
+
+// Handle POST requests to /add with the upload middleware
+app.post('/add', upload.single('stlfile'), (req, res) => {
+  // req.file contains the uploaded file information
+  if (req.file) {
+    console.log('File uploaded successfully:', req.file.filename);
+  } else {
+    console.log('No file uploaded.');
+  }
+
+  res.send('Transmission completed!');
+});
